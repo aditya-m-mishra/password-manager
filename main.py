44542ac7b1819ee456041de22eb9900ca484d0ba
aditya-m-mishra <adiapplies@gmail.com>
2026-04-23 14:06:41 +0530

    logo added

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from tkinter import *
 
 window = Tk()
-window.minsize(height=500, width=700)
+window.minsize(height=300, width=500)
 window.title("password manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(height=250, width=250)
+logo_img = PhotoImage(file="password logo.png")
+canvas.create_image(125, 125, image=logo_img)
+canvas.pack()
 
 window.mainloop()
